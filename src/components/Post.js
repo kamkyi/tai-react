cdbbe253eff4f018ac40377d0fbcb30a51a66d5a
Thiha <waihynhtun1994@gmail.com>
2020-12-14 22:53:43 +0630

    INitial Commit

---
+++ src/components/Post.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Post.css'
+
+class Post extends Component{
+    render() {
+        return (
+            <div className="Post">
+              <h1>I'm Post</h1> 
+            </div>
+        );
+    }
+}
+
+export default Post;
